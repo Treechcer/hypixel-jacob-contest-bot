a103ceac42ea4a33dd16f790c5a4a2730e301554
treecher <treechcer@gmail.com>
2026-02-21 01:02:19 +0100

    seems to work with more servers?

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import { Client, Events, GatewayIntentBits } from 'discord.js';
 
 import "dotenv/config"
 
-import { initDB, addServer} from "./src/database.js";
+import { initDB, addServer, sendMessageToAllChannels } from "./src/database.js";
 
 const token = process.env.TOKEN_DS;
-const guildID = process.env.GUILD;
+//const guildID = process.env.GUILD;
+
+let time = 9999999999
 
 var roles = {
     //name : id
@@ -35,56 +37,76 @@ client.once(Events.ClientReady, (readyClient) => {
             }
 
             const result = await response.json();
-            //console.log(result);
-
-            //console.log(result[0].timestamp - new Date().getTime())
-
-            let time = result[0].timestamp - new Date().getTime()
-
-            const guild = client.guilds.cache.get(guildID);
-            if (!guild) return;
-
-            for (let i = 0; i < result[0].cropNames.length; i++) {
-                let roleName = result[0].cropNames[i];
-
-                let role = guild.roles.cache.find(x => x.name === roleName);
-
-                if (!role) {
-                    role = await guild.roles.create({
-                        name: roleName,
-                        color: 0x00ff00,
-                        permissions: []
-                    });
-                }
-
-                roles[roleName] = role.id;
-            }
+            console.log("Fetched contests, next timestamp:", result[0].timestamp);
 
             //console.log(result[0])  
 
+            time = result[0].timestamp - new Date().getTime()
+
+            //console.log(result[0].timestamp - new Date().getTime())
+
             nextCrops = result[0].cropNames;
 
-            setTimeout(async() => {
-                console.log("!!!!!")
-                const channel = await client.channels.fetch("1474462322335420450");
-                let msg = "Contest starts on <t:" + Math.floor(result[0].timestamp / 1000) + ":R> \n"
-                for (let i = 0; i < nextCrops.length; i++){
-                    msg += "<@&" + roles[nextCrops[i]] + "> "
-                }
-
-                await channel.send(msg);
-            }, (time) < 180000 ? 1 : 1)
+            sendMessageToAllChannels(nextCrops, result[0].timestamp)
         } 
         catch (error) {
             console.error(error.message);
         }
 
+        roles = {}
+
         //const channel = await client.channels.fetch("1474462322335420450");
         //
         //const data = await getData();
         //await channel.send(data);
-    }, 1000 * 60 * 20 /*5000*/) // 20 minutes
+    }, /*1000 * 60 * 20*/ 5000) // 20 minutes
 });
+
+export async function checkRoles(guildID, crops){
+    const guild = client.guilds.cache.get(guildID);
+    if (!guild) return;
+
+    for (let i = 0; i < crops.length; i++) {
+        let roleName = crops[i];
+
+        let role = guild.roles.cache.find(x => x.name === roleName);
+
+        if (!role) {
+            role = await guild.roles.create({
+                name: roleName,
+                color: 0x00ff00,
+                permissions: []
+            });
+        }
+        roles[guildID] = roles[guildID] || {}
+        roles[guildID][roleName] = role.id;
+    }
+}
+
+export async function sendMessage(channelID, guildID, timestamp){
+    let timeRemaining = timestamp - new Date().getTime();
+    setTimeout(async() => {
+        try{
+            const channel = await client.channels.fetch(channelID);
+
+            if (!channel) {
+                console.log("Channel not found");
+                return;
+            }
+
+            let msg = "Contest starts on <t:" + Math.floor(timestamp / 1000) + ":R> \n"
+            for (let i = 0; i < nextCrops.length; i++){
+                msg += "<@&" + roles[guildID][nextCrops[i]] + "> "
+            }
+            
+            await channel.send(msg);
+            console.log("Message sent");
+        }
+        catch (err) {
+            console.error("Error sending message:", err)
+        }
+    }, (timeRemaining) < 180000 ? 1 : timeRemaining)
+}
 
 client.on("messageCreate", message => {
     if (message.author.bot) return;
--- src/database.js
+++ src/database.js
@@ -1,12 +1,28 @@
 import sqlite3 from 'sqlite3';
+import { sendMessage, checkRoles } from "../index.js"
 
 const db = new sqlite3.Database('./servers.db');
 
 export function initDB(){
-    db.run("create table if not EXISTS test(guildID INTEGER PRIMARY KEY, channelID integer);");
+    db.run(`create table if not EXISTS servers(
+        guildID TEXT PRIMARY KEY UNIQUE NOT NULL,
+        channelID TEXT
+    );`);
 }
 
 export function addServer(guilID, channelID){
-    db.run(`INSERT INTO test VALUES(${guilID}, ${channelID});`);
+    db.run(`INSERT OR REPLACE INTO servers VALUES(?, ?);`, [guilID, channelID]);
 }
 
+export function sendMessageToAllChannels(crops, timestamp){
+    db.each(`SELECT * FROM servers;`, async (err, row) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        console.log(`Sending to guild ${row.guildID}, channel ${row.channelID}`);
+        await checkRoles(row.guildID, crops);
+        await sendMessage(row.channelID, row.guildID, timestamp);
+    });
+}
