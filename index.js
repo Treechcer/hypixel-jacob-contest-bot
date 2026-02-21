64bafb02b0a9528cb37fc697f86c81f583b005cb
treecher <treechcer@gmail.com>
2026-02-21 01:53:48 +0100

    started conserving API calls so I won't (hopefully) don't get blocked IP lol

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ import "dotenv/config"
 
 import { initDB, addServer, sendMessageToAllChannels } from "./src/database.js";
 
+var globalResponse = false
+
 const token = process.env.TOKEN_DS;
 //const guildID = process.env.GUILD;
 
-let time = 9999999999
+var time = 9999999999
 
 var roles = {
     //name : id
@@ -31,35 +33,40 @@ client.once(Events.ClientReady, (readyClient) => {
     setInterval(async() => {
         const url = "https://jacobs.strassburger.dev/api/jacobcontests";
         try {
-            const response = await fetch(url, {method : "GET"});
-            if (!response.ok) {
-                throw new Error(`Response status: ${response.status}`);
+            if (!globalResponse || !globalResponse.length){
+                const response = await fetch(url, {method : "GET"});
+                if (!response.ok) {
+                    throw new Error(`Response status: ${response.status}`);
+                }
+
+                const result = await response.json();
+                globalResponse = result
             }
 
-            const result = await response.json();
-            console.log("Fetched contests, next timestamp:", result[0].timestamp);
+            console.log("Fetched contests, next timestamp:", globalResponse[0].timestamp);
 
             //console.log(result[0])  
 
-            time = result[0].timestamp - new Date().getTime()
+            time = globalResponse[0].timestamp - new Date().getTime()
 
             //console.log(result[0].timestamp - new Date().getTime())
 
-            nextCrops = result[0].cropNames;
+            nextCrops = globalResponse[0].cropNames;
 
-            sendMessageToAllChannels(nextCrops, result[0].timestamp)
+            sendMessageToAllChannels(nextCrops, globalResponse[0].timestamp)
         } 
         catch (error) {
             console.error(error.message);
         }
 
         roles = {}
+        globalResponse.shift()
 
         //const channel = await client.channels.fetch("1474462322335420450");
         //
         //const data = await getData();
         //await channel.send(data);
-    }, /*1000 * 60 * 20*/ 5000) // 20 minutes
+    }, 1000 * 60 * 20 /*2000*/) // 20 minutes
 });
 
 export async function checkRoles(guildID, crops){
