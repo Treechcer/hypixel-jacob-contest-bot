af8b4fde6ab2fa1736be433a1df03e27de30c3d4
treecher <treechcer@gmail.com>
2026-02-21 22:50:35 +0100

    added weekly stats (because why not lol)

--- src/database.js
+++ src/database.js
@@ -8,6 +8,11 @@ export function initDB(){
         guildID TEXT PRIMARY KEY UNIQUE NOT NULL,
         channelID TEXT
     );`);
+
+    db.run(`create table if not EXISTS weeklyContests(
+        crop TEXT PRIMARY KEY UNIQUE NOT NULL,
+        count INTEGER
+    );`);
 }
 
 export function addServer(guilID, channelID){
@@ -25,4 +30,17 @@ export async function sendMessageToAllChannels(crops, timestamp){
         await checkRoles(row.guildID, crops);
         await sendMessage(row.channelID, row.guildID, timestamp);
     });
+
+    if (new Date().getDay() == 0){
+        db.each(`SELECT * FROM weeklyContests;`, async (err, row) => {
+            db.run(`UPDATE weeklyContests set count = 0 where crop = ?`, [row.crop]);
+        })
+    }
+
+    for (let i = 0; i < crops.length; i ++){
+        let cropName = crops[i].replace(" ", "-").trim()
+        //console.log("AAAA" + cropName)
+        db.run(`INSERT OR IGNORE INTO weeklyContests VALUES(?,?)`, [cropName, 0])
+        db.run(`UPDATE weeklyContests set count = count + 1 WHERE crop = ?`, [cropName])
+    }
 }
